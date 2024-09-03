40e30fb0166ae9f6194b7fe1dd8bd7ae838edfbc
lyaneii <kwchu@DESKTOP-TRCHBSV.localdomain>
2024-09-03 17:55:48 +0200

    style: add colours for test output

---
+++ src/Config/colours.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   colours.hpp                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: kwchu <kwchu@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/04/14 15:54:52 by kwchu         #+#    #+#                 */
+/*   Updated: 2024/08/22 14:58:57 by kwchu         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CPP_COLOURS_HPP
+# define CPP_COLOURS_HPP
+
+# define RED "\033[1;31m"
+# define GREEN "\033[1;32m"
+# define YELLOW "\033[1;33m"
+# define BLUE "\033[1;34m"
+# define MAGENTA "\033[1;35m"
+# define CYAN "\033[1;36m"
+# define ORANGE "\033[1;38;5;214m"
+# define GREY "\033[1;38;5;240m"
+# define RESET "\033[0m"
+
+#endif
